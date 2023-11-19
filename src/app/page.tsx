68e971fc07575238166f0e457cc3eb3fd8e4d808
Aakash Bhadu <ajat31535@gmail.com>
2023-11-19 21:50:48 +0530

    some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,7 @@ import TopPlaces from '@/components/topplaces'
 export default function Home() {
   return (
     <main className="flex min-h-screen min-w-full flex-col items-center justify-around px-24 py-2">
-      <SlideShow className="w-full h-56 overflow-hidden rounded-lg md:h-96">
-        <NextMatch />
+      <SlideShow className="w-full h-64 overflow-hidden rounded-lg md:h-1/3">
         <NextMatch />
       </SlideShow>
       <UpcomingMatches />
